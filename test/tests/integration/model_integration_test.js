928015e171a8429fca3dbc26c721c9b0c1e4e2b2
Dan Gebhardt <dan@cerebris.com>
2014-05-23 22:13:57 -0400

    WIP - Model integration tests - creating and updating record

--- test/tests/integration/model_integration_test.js
+++ test/tests/integration/model_integration_test.js
@@ -2,6 +2,7 @@ import Orbit from 'orbit';
 import attr from 'ember_orbit/attr';
 import hasOne from 'ember_orbit/has_one';
 import hasMany from 'ember_orbit/has_many';
+import Context from 'ember_orbit/context';
 import Store from 'ember_orbit/store';
 import Model from 'ember_orbit/model';
 import { createStore } from 'test_helper';
@@ -10,7 +11,8 @@ var get = Ember.get;
 var Planet,
     Moon,
     Star,
-    store;
+    store,
+    context;
 
 module("Integration - Model", {
   setup: function() {
@@ -40,10 +42,19 @@ module("Integration - Model", {
         planet: Planet
       }
     });
+
+    context = Context.create({
+      store: store
+    });
   },
 
   teardown: function() {
+    Orbit.Promise = null;
+    Star = null;
+    Moon = null;
     Planet = null;
+    store = null;
+    context = null;
   }
 });
 
@@ -55,4 +66,30 @@ test("store is properly linked to models", function() {
   equal(store.modelFor('star'), Star);
   equal(store.modelFor('planet'), Planet);
   equal(store.modelFor('moon'), Moon);
+});
+
+test("new models can be created", function() {
+  expect(4);
+
+  Ember.run(function() {
+    stop();
+    context.add('planet', {name: 'Earth'}).then(function(planet) {
+      equal(planet.get('name'), 'Earth');
+
+      planet.set('name', 'Jupiter');
+
+      equal(planet.get('name'), 'Jupiter', 'CP reflects transformed value');
+
+      equal(context._source.retrieve(['planet', planet.get('clientid'), 'name']),
+            'Earth',
+            'memory source patch is not yet complete');
+
+      context.then(function() {
+        start();
+        equal(context._source.retrieve(['planet', planet.get('clientid'), 'name']),
+              'Jupiter',
+              'memory source patch is now complete');
+      });
+    });
+  });
 });
