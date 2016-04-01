443260e2eec2fde79a508dd85aa4701fb4f1f295
Dan Gebhardt <dan@cerebris.com>
2016-04-01 15:19:02 -0400

    Simplify LiveQuery.
    Minimize use of get/set on instance-level vars, favoring direct access
    where possible.

--- addon/live-query.js
+++ addon/live-query.js
@@ -1,4 +1,7 @@
 import ReadOnlyArrayProxy from 'ember-orbit/system/read-only-array-proxy';
+import Ember from 'ember';
+
+const { get, set } = Ember;
 
 export default ReadOnlyArrayProxy.extend({
   _orbitCache: null,
@@ -6,17 +9,15 @@ export default ReadOnlyArrayProxy.extend({
   _identityMap: null,
 
   init(...args) {
-    console.debug('creating liveQuery', this);
-    this.set('content', Ember.A());
-
+    // console.debug('creating liveQuery', this);
     this._super(...args);
 
-    const orbitCache = this.get('_orbitCache');
-    const query = this.get('_query');
-    const orbitLiveQuery = orbitCache.liveQuery(query);
+    set(this, 'content', Ember.A());
+
+    const orbitLiveQuery = this._orbitCache.liveQuery(this._query);
 
     orbitLiveQuery.subscribe((operation) => {
-      console.debug('liveQuery', operation);
+      // console.debug('liveQuery', operation);
 
       const handler = `_${operation.op}`;
 
@@ -30,17 +31,15 @@ export default ReadOnlyArrayProxy.extend({
 
   _addRecord(operation) {
     const record = this._recordFor(operation);
-    this.get('content').pushObject(record);
+    get(this, 'content').pushObject(record);
   },
 
   _removeRecord(operation) {
     const record = this._recordFor(operation);
-    this.get('content').removeObject(record);
+    get(this, 'content').removeObject(record);
   },
 
   _recordFor(operation) {
-    const identityMap = this.get('_identityMap');
-    const { type, id } = operation.record;
-    return identityMap.lookup({id, type});
+    return this._identityMap.lookup(operation.record);
   }
 });
