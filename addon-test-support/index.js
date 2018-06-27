d016717d4102660c1c971987839a72abefce8bd5
Miguel Camba <miguel.camba@gmail.com>
2018-06-27 15:25:26 +0200

    If the source doesn't have pending tasks, resolve immediately.

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -5,10 +5,14 @@ export async function waitForSource(sourceName) {
   let { owner } = getContext();
   let source = owner.lookup(`data-source:${sourceName}`);
   if (source) {
-    return new Promise(function(resolve, reject) {
-      source.syncQueue.on('complete', resolve);
-      source.syncQueue.on('fail', reject);
-    });
+    if (source.syncQueue.empty) {
+      return Promise.resolve();
+    } else {
+      return new Promise(function(resolve, reject) {
+        source.syncQueue.on('complete', resolve);
+        source.syncQueue.on('fail', reject);
+      });
+    }
   } else {
     throw new Error(`data-source:${sourceName} not found. Maybe you misspelled it?`);
   }
