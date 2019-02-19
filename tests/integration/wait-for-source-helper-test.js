00f3a405ef4efeed3b0cb5257cf79c2801413b14
Dan Gebhardt <dan@cerebris.com>
2019-02-19 10:50:03 -0500

    Expand tests for waitForSource helper

--- tests/integration/wait-for-source-helper-test.js
+++ tests/integration/wait-for-source-helper-test.js
@@ -1,47 +1,51 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { Planet } from 'dummy/tests/support/dummy-models';
-import EmberObject from '@ember/object';
 import { createStore } from 'dummy/tests/support/store';
-import { Promise as EmberPromise } from 'rsvp';
 import { waitForSource } from 'ember-orbit/test-support';
-import { Source } from '@orbit/data';
-
-class TestSource extends Source {
-
-}
 
 module('waitForSource helper', function(hooks) {
   let store;
+
   setupTest(hooks);
 
   hooks.beforeEach(function () {
-    this.owner.register("data-source:backup", {
-      create(injections) {
-        return new TestSource(injections);
-      }
-    });
     store = createStore({ models: { planet: Planet } });
   });
 
-  test('it resolves once all the pending requests to the given source have synced', async function(_assert) {
-    let RequesterObj = EmberObject.extend({
-      init() {
-        this._super(...arguments);
-        this.set('planets', []);
-        this.fetchAndUpdate();
-      },
+  test('it resolves once all the pending requests to the given source have synced', async function(assert) {
+    const backup = createStore({ models: { planet: Planet } });
 
-      async fetchAndUpdate() {
-        let planets = await store.liveQuery(q => q.findRecords('planet'));
-        this.set('planets', planets.map(p => p.name));
-      }
-    })
-    await EmberPromise.all([
-      store.addRecord({ type: 'planet', name: 'Earth' }),
-      store.addRecord({ type: 'planet', name: 'Mercury' })
-    ]);
-    RequesterObj.create();
+    store.on('update', (transform) => {
+      backup.update(transform);
+    });
+
+    await store.addRecord({ type: 'planet', name: 'Earth' });
+
+    assert.notOk(backup.source.requestQueue.empty);
+
+    await waitForSource(backup);
+
+    assert.ok(backup.source.requestQueue.empty);
+    assert.ok(backup.source.syncQueue.empty);
+  });
+
+  test('it looks up data sources by name if a string is provided', async function(assert) {
+    const backup = createStore({ models: { planet: Planet } });
+
+    this.owner.register('data-source:backup', backup, { instantiate: false });
+
+    store.on('update', (transform) => {
+      backup.update(transform);
+    });
+
+    await store.addRecord({ type: 'planet', name: 'Earth' });
+
+    assert.notOk(backup.source.requestQueue.empty);
+
     await waitForSource('backup');
+
+    assert.ok(backup.source.requestQueue.empty);
+    assert.ok(backup.source.syncQueue.empty);
   });
 });
