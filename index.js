266cb3fffb203a8fabfaaa1693a158ecc0573e62
Paul Chavard <git@paul.chavard.net>
2019-07-07 13:32:10 +0200

    Validate presence of models collection

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 'use strict';
+const path = require('path');
+const assert = require('assert');
+const fs = require('fs');
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+  included() {
+    const app = this._findHost();
+    const addonConfig = this.app.project.config(app.env)['orbit'] || {};
+    const collections = addonConfig.collections || {};
+    let modelsPath;
+
+    if (collections.models) {
+      modelsPath = path.join('app', collections.models);
+    } else if (
+      app.project.pkg['ember-addon'] &&
+      app.project.pkg['ember-addon'].configPath
+    ) {
+      modelsPath = path.join('tests', 'dummy', 'app', 'data-models');
+    } else {
+      modelsPath = path.join('app', 'data-models');
+    }
+
+    const modelsDirectory = path.join(app.project.root, modelsPath);
+
+    assert(
+      fs.existsSync(modelsDirectory),
+      `You need to create models directory: "${modelsDirectory}"`
+    );
+
+    this._super.included.apply(this, arguments);
+  }
 };
