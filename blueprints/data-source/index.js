9bb732765782e15f3e708e2f31240af35b749fac
Dan Gebhardt <dan@cerebris.com>
2019-07-26 16:04:46 -0400

    data-source generator will now add any specified packages
    Running `ember g data-source --from="@orbit/jsonapi"` will now add the `@orbit/jsonapi` source to the users project.

--- blueprints/data-source/index.js
+++ blueprints/data-source/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const DEFAULT_PACKAGE = 'TODO';
+
 module.exports = {
   description: 'Generates an ember-orbit source.',
 
-  availableOptions: [{ name: 'from', type: String, default: 'TODO' }],
+  availableOptions: [{ name: 'from', type: String, default: DEFAULT_PACKAGE }],
 
   fileMapTokens() {
     const addonConfig = this.project.config()['orbit'] || {};
@@ -19,5 +21,11 @@ module.exports = {
   locals(options) {
     const { from } = options;
     return { from };
+  },
+
+  afterInstall(options) {
+    if (options.from && options.from !== DEFAULT_PACKAGE) {
+      return this.addPackageToProject(options.from);
+    }
   }
 };
