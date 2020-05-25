87472a5670cc9eb03ecf7f2ab34648b484d64af6
Paul Chavard <git@paul.chavard.net>
2020-05-25 19:04:21 +0200

    drop pre-octane ember support

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,22 +12,6 @@ module.exports = function () {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-3.8',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.8.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-3.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.12.0'
-            }
-          }
-        },
-        {
           name: 'ember-lts-3.16',
           npm: {
             devDependencies: {
