67d51d2cfaaf2388bec025be84f8cd2bea35a08c
Derek W. S. Gray <github@mudflye.org>
2020-06-06 09:15:11 -0400

    Support polymorphic relationships. Issue #257 (#262)
    * Support polymorphic relationships. Issue #257
    
    * Prettier lint somehow missed.
    
    * PR Feedback: Assert RecordIdentities for polymorphic relationships

--- addon/-private/utils/normalize-record-properties.ts
+++ addon/-private/utils/normalize-record-properties.ts
@@ -1,4 +1,4 @@
-import {
+import Orbit, {
   Schema,
   RecordRelationship,
   Record,
@@ -6,6 +6,8 @@ import {
   RecordIdentity
 } from '@orbit/data';
 import { deepSet, Dict } from '@orbit/utils';
+
+const { assert } = Orbit;
 
 export default function normalizeRecordProperties(
   schema: Schema,
@@ -56,7 +58,9 @@ function assignRelationships(
   const relationships = modelDefinition.relationships || {};
   for (let relationship of Object.keys(relationships)) {
     if (properties[relationship] !== undefined) {
-      let relationshipType = relationships[relationship].type as string;
+      let relationshipType = relationships[relationship].type as
+        | string
+        | string[];
       let relationshipProperties = properties[relationship] as
         | RecordIdentity
         | RecordIdentity[]
@@ -73,26 +77,36 @@ function assignRelationships(
 }
 
 function normalizeRelationship(
-  type: string,
+  type: string | string[],
   value: RecordIdentity | RecordIdentity[] | string | string[] | null
 ): RecordRelationship {
   const relationship: RecordRelationship = {};
+
+  const isPolymorphic = Array.isArray(type);
 
   if (isHasMany(value)) {
     relationship.data = [];
     for (let identity of value) {
       if (typeof identity === 'string') {
-        relationship.data.push({ type, id: identity });
+        assert(
+          'The hasMany relationship is polymorphic, so string[] will not work as a value. RecordIdentity[] must be provided for type information.',
+          !isPolymorphic
+        );
+        relationship.data.push({ type: type as string, id: identity });
       } else {
-        relationship.data.push({ type, id: identity.id });
+        relationship.data.push({ type: identity.type, id: identity.id });
       }
     }
   } else if (value === null) {
     relationship.data = null;
   } else if (typeof value === 'string') {
-    relationship.data = { type, id: value };
+    assert(
+      'The relationship is polymorphic, so string will not work as a value. RecordIdentity must be provided for type information.',
+      !isPolymorphic
+    );
+    relationship.data = { type: type as string, id: value };
   } else {
-    relationship.data = { type, id: value.id };
+    relationship.data = { type: value.type, id: value.id };
   }
 
   return relationship;
