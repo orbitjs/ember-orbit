6f5b52127a8451e2136e52d423f5cb2e7be23f2b
Paul Chavard <github@paul.chavard.net>
2020-09-25 13:17:13 +0200

    Update dependencies

--- addon/-private/utils/glimmer-cache.ts
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export function createCache<T>(fn: () => T): object {
-  return (Ember as any)._createCache(fn) as object;
-}
-
-export function getValue<T>(cache: object): T {
-  return (Ember as any)._cacheGetValue(cache) as T;
-}
