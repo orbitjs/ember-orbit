5b8c8f1afee3e568d1b6265c8495123256a479cf
Robert Wagner <rwwagner90@gmail.com>
2021-01-19 21:20:20 -0500

    Update undo-manager.js

--- tests/dummy/app/services/undo-manager.js
+++ tests/dummy/app/services/undo-manager.js
@@ -5,11 +5,9 @@ function removeFromTo(array, from, to) {
   array.splice(
     from,
     !to ||
-      // @ts-expect-error: We need to refactor this function
       1 +
         to -
         from +
-        // @ts-expect-error: We need to refactor this function
         (!((to < 0) ^ (from >= 0)) && (to < 0 || -1) * array.length)
   );
   return array.length;
